772691293d292627a9e75d30812743c4030387f5
Stephan Tarulli <srt@tinychameleon.com>
2021-12-05 22:34:00 -0800

    Solution for Day #6

---
+++ src/advent_of_code_2021/day6.clj
@@ -0,0 +1,50 @@
+(ns advent-of-code-2021.day6
+  (:require [advent-of-code-2021.core :as core]
+            [clojure.string :as str]))
+
+(def ^:const spawn-ready-age 0)
+(def ^:const newly-spawned-age 8)
+(def ^:const cycle-reset-age 6)
+
+(defn parse-lanternfish-ages
+  [filename]
+  (->> (core/read-file filename)
+       str/trim
+       (core/str-split #",")
+       (mapv core/str->int)
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (into {})))
+
+(defn simulate-age
+  [age]
+  (if (= age 0) cycle-reset-age (dec age)))
+
+(def safe+ (fnil + 0))
+
+(defn update-population
+  [population k v]
+  (update population k safe+ v))
+
+(defn simulate-population
+  [population]
+  (reduce (fn [pop [k v]]
+            (update-population pop (simulate-age k) v))
+          {}
+          population))
+
+(defn simulate-cycle
+  [population]
+  (let [new-fish (get population spawn-ready-age 0)]
+    (update-population (simulate-population population) newly-spawned-age new-fish)))
+
+(defn solver
+  [days]
+  (let [population (parse-lanternfish-ages "input/day6.txt")
+        simulation (iterate simulate-cycle population)
+        result (first (drop days simulation))]
+    (reduce + (vals result))))
+
+(def part-1 (partial solver 80))
+
+(def part-2 (partial solver 256))
